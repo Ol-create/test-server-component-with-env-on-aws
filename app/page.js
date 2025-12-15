9e753ab9be2049232a6d3bb3aabf89855ea86aa8
Ol-create <oluola96@gmail.com>
2025-12-15 17:09:34 +0100

    first commit

--- app/page.js
+++ app/page.js
@@ -1,6 +1,7 @@
 import Image from "next/image";
 
 export default function Home() {
+  const API_URL = process.env.API_URL;
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
@@ -58,6 +59,7 @@ export default function Home() {
           >
             Documentation
           </a>
+          <p>SERVER API URL: {API_URL}</p>
         </div>
       </main>
     </div>
